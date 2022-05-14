57d1128d2a33ce241cdece73f520ff2d063e8e90
Melissa Mojica <melissa_mojica23@hotmail.com>
2022-05-14 16:39:11 -0400

    1.3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    
+    private Rigidbody2D rb;
+    
+    public bool facingRight = true;
+    
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    public LayerMask whatIsGround2;
+    
+    private bool top = false;
+    
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate(){
+        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        if (isGrounded == false) {
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround2);
+        }
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);
+        
+        if(facingRight == false && moveInput > 0){
+            reflect();
+        } else if (facingRight == true && moveInput <0){
+            reflect();
+        }
+    }
+    
+    void Update(){
+        jump();
+    }
+    
+    void jump(){
+        if(Input.GetKeyDown(KeyCode.W) && isGrounded == true||Input.GetKeyDown(KeyCode.Space) && isGrounded == true){
+                rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+    
+    void reflect(){
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+    
+    void Rotation(){
+        if(top == false){
+            transform.eulerAngles = new Vector3(0,0,180f);
+        } else {
+            transform.eulerAngles = Vector3.zero;
+        }
+        
+        facingRight = !facingRight;
+        top = !top;
+    }
+}
---
+++ Assets/Scripts/WallController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D (Collider2D col) {
+        // if (col.gameObject.name == "Fire") {
+        //     var fireVelocity = col.gameObject.GetComponent<Rigidbody2D>().velocity;
+        //     col.gameObject.GetComponent<Rigidbody2D>().velocity = (fireVelocity * -1);
+        // }
+    }
+}
